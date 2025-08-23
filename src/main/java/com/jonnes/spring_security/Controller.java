7ff2b3858049337a59c71d0a5aed7010d55c7fbc
Jonnes <jonnes400@gmail.com>
2025-08-23 19:32:03 +0530

    initial set up of spring security project

---
+++ src/main/java/com/jonnes/spring_security/Controller.java
@@ -0,0 +1,14 @@
+package com.jonnes.spring_security;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("sample")
+    public String sample(){
+        return "sample";
+    }
+}
