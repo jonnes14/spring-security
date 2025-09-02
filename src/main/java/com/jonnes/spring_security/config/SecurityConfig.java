a51682077c1b89ec257031d0f28931125ce99be8
Jonnes <jonnes400@gmail.com>
2025-09-02 10:56:28 +0530

    tried to authenticate via UserDetailsService

--- src/main/java/com/jonnes/spring_security/config/SecurityConfig.java
+++ src/main/java/com/jonnes/spring_security/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -13,11 +17,22 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http.csrf(customizer-> customizer.disable()); //disable the csrf
-            http.authorizeHttpRequests(request-> request.anyRequest().authenticated()); // to enable username and password
-            http.formLogin(Customizer.withDefaults()); // no need form login becuase its stateless
-            http.httpBasic(Customizer.withDefaults()); // to provide form login
-            http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // to make sessionn stateless
+            http
+                    .csrf(customizer-> customizer.disable()) //disable the csrf
+                    .authorizeHttpRequests(request-> request.anyRequest().authenticated()) // to enable username and password
+                     //.formLogin(Customizer.withDefaults()); // no need form login becuase its stateless
+                     .httpBasic(Customizer.withDefaults()) // to provide form login
+                     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // to make sessionn stateless
             return http.build();
+    }
+    //below method is to authenticate multiple users
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user= User.withDefaultPasswordEncoder()
+                              .username("jonnes")
+                              .password("jonnes@1")
+                              .roles("USER")
+                              .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
