ad98edb769f39e0c58d30be9b05e3dbbfa1be102
Jonnes <jonnes400@gmail.com>
2025-09-02 08:44:56 +0530

    added security config

--- src/main/java/com/jonnes/spring_security/config/SecurityConfig.java
+++ src/main/java/com/jonnes/spring_security/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.jonnes.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -11,6 +13,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.csrf(customizer-> customizer.disable()); //disable the csrf
+            http.authorizeHttpRequests(request-> request.anyRequest().authenticated()); // to enable username and password
+            http.formLogin(Customizer.withDefaults()); // no need form login becuase its stateless
+            http.httpBasic(Customizer.withDefaults()); // to provide form login
+            http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // to make sessionn stateless
             return http.build();
     }
 }
