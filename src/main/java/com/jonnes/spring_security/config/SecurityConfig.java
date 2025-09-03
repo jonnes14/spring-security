8f2820b74221e33571f07d988b0a8fc0abded347
Jonnes <jonnes400@gmail.com>
2025-09-03 22:41:53 +0530

    userdetailsservice

--- src/main/java/com/jonnes/spring_security/config/SecurityConfig.java
+++ src/main/java/com/jonnes/spring_security/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.jonnes.spring_security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,12 +12,24 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance();
+        return provider;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             http
@@ -26,13 +41,13 @@ public class SecurityConfig {
             return http.build();
     }
     //below method is to authenticate multiple users
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user= User.withDefaultPasswordEncoder()
-                              .username("jonnes")
-                              .password("jonnes@1")
-                              .roles("USER")
-                              .build();
-        return new InMemoryUserDetailsManager(user);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        UserDetails user= User.withDefaultPasswordEncoder()
+//                              .username("jonnes")
+//                              .password("jonnes@1")
+//                              .roles("USER")
+//                              .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
 }
