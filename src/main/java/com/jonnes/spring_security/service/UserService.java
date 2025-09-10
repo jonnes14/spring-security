a47434e58256ef43a64fd0605dbd49dd40c2b3dc
Jonnes <jonnes400@gmail.com>
2025-09-10 09:57:40 +0530

    setted up the encoder

--- src/main/java/com/jonnes/spring_security/service/UserService.java
+++ src/main/java/com/jonnes/spring_security/service/UserService.java
@@ -3,6 +3,7 @@ package com.jonnes.spring_security.service;
 import com.jonnes.spring_security.dao.UserRepo;
 import com.jonnes.spring_security.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,10 @@ public class UserService {
 
     @Autowired
     private UserRepo repo;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
 
     public User saveUser(User user){
+        user.setPassword(encoder.encode(user.getPassword()));
        return repo.save(user);
     }
 
