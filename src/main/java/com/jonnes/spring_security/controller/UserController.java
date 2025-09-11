5629159283f8577d9164c3adab9447dad9f78c4d
Jonnes <jonnes400@gmail.com>
2025-09-11 09:56:16 +0530

    jwt string auth

--- src/main/java/com/jonnes/spring_security/controller/UserController.java
+++ src/main/java/com/jonnes/spring_security/controller/UserController.java
@@ -4,6 +4,9 @@ package com.jonnes.spring_security.controller;
 import com.jonnes.spring_security.model.User;
 import com.jonnes.spring_security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +17,21 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     @PostMapping("register")
     public User getUser (@RequestBody User user){
         return service.saveUser(user);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+        Authentication authentication= authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+
+        if(authentication.isAuthenticated()){
+            return "Success";
+        }
+        else return "failed";
     }
 }
