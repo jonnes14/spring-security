c4ff3f9a6a3a9bbdd3f786558d5a4ff8df23f612
Jonnes <jonnes400@gmail.com>
2025-09-16 23:00:03 +0530

    the generate token validity

--- src/main/java/com/jonnes/spring_security/service/JwtService.java
+++ src/main/java/com/jonnes/spring_security/service/JwtService.java
@@ -1,10 +1,22 @@
 package com.jonnes.spring_security.service;
 
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
 
 @Service
 public class JwtService {
     public String generateToken(String username) {
+        HashMap<String,Object> claims=new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*3))
+                .signWith(getkey(), SignatureAlgorithm.HS256).compact();
     }
 }
